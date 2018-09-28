02a7c947c8cb84add00524233c8e232c94bff622
Max Prosper <maxim.yaaman@gmail.com>
2018-09-28 23:19:05 +0300

    Abstract Factory pattern

---
+++ creational/abstract_factory.go
@@ -0,0 +1,134 @@
+package creational
+
+import "fmt"
+
+// ************** VEHICLE FACTORY ************** //
+
+type Vehicle interface {
+	NumWheels() int
+	NumSeats() int
+}
+
+type Car interface {
+	NumDoors() int
+}
+
+type Motorbike interface {
+	GetMotorbikeType() int
+}
+
+type VehicleFactory interface {
+	Build(v int) (Vehicle, error)
+}
+
+const (
+	CarFactoryType       = 1
+	MotorbikeFactoryType = 2
+)
+
+func BuildFactory(f int) (VehicleFactory, error) {
+	switch f {
+	case CarFactoryType:
+		return new(CarFactory), nil
+	case MotorbikeFactoryType:
+		return new(MotorbikeFactory), nil
+	default:
+		return nil, fmt.Errorf("Factory with id %d not recognized\n", f)
+	}
+}
+
+// ************** CAR FACTORY ************** //
+
+const (
+	LuxuryCarType = 1
+	FamilyCarType = 2
+)
+
+type CarFactory struct{}
+
+func (c *CarFactory) Build(v int) (Vehicle, error) {
+	switch v {
+	case LuxuryCarType:
+		return new(LuxuryCar), nil
+	case FamilyCarType:
+		return new(FamilyCar), nil
+	default:
+		return nil, fmt.Errorf("Vehicle of type %d not recognized\n", v)
+	}
+}
+
+type LuxuryCar struct{}
+
+func (*LuxuryCar) NumDoors() int {
+	return 4
+}
+
+func (*LuxuryCar) NumWheels() int {
+	return 4
+}
+
+func (*LuxuryCar) NumSeats() int {
+	return 5
+}
+
+type FamilyCar struct{}
+
+func (*FamilyCar) NumDoors() int {
+	return 5
+}
+
+func (*FamilyCar) NumWheels() int {
+	return 4
+}
+
+func (*FamilyCar) NumSeats() int {
+	return 5
+}
+
+// ************** BIKE FACTORY ************** //
+
+const (
+	SportMotorbikeType  = 1
+	CruiseMotorbikeType = 2
+)
+
+type MotorbikeFactory struct{}
+
+func (m *MotorbikeFactory) Build(v int) (Vehicle, error) {
+	switch v {
+	case SportMotorbikeType:
+		return new(SportMotorbike), nil
+	case CruiseMotorbikeType:
+		return new(CruiseMotorbike), nil
+	default:
+		return nil, fmt.Errorf("Vehicle of type %d not recognized\n", v)
+	}
+}
+
+type SportMotorbike struct{}
+
+func (s *SportMotorbike) NumWheels() int {
+	return 2
+}
+
+func (s *SportMotorbike) NumSeats() int {
+	return 1
+}
+
+func (s *SportMotorbike) GetMotorbikeType() int {
+	return SportMotorbikeType
+}
+
+type CruiseMotorbike struct{}
+
+func (c *CruiseMotorbike) NumWheels() int {
+	return 2
+}
+
+func (c *CruiseMotorbike) NumSeats() int {
+	return 2
+}
+
+func (c *CruiseMotorbike) GetMotorbikeType() int {
+	return CruiseMotorbikeType
+}
---
+++ creational/abstract_factory_test.go
@@ -0,0 +1,44 @@
+package creational
+
+import "testing"
+
+func TestMotorbikeFactory(t *testing.T) {
+	motorbikeF, err := BuildFactory(MotorbikeFactoryType)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	motorbikeVehicle, err := motorbikeF.Build(SportMotorbikeType)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("Motorbike vehicle has %d wheels\n", motorbikeVehicle.NumWheels())
+
+	sportBike, ok := motorbikeVehicle.(Motorbike)
+	if !ok {
+		t.Fatal("Struct assertion has failed")
+	}
+
+	t.Logf("Sport motorbike has type %d\n", sportBike.GetMotorbikeType())
+}
+
+func TestCarFactory(t *testing.T) {
+	carF, err := BuildFactory(CarFactoryType)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	carVehicle, err := carF.Build(LuxuryCarType)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("Car vehicle has %d seats\n", carVehicle.NumSeats())
+
+	luxuryCar, ok := carVehicle.(Car)
+	if !ok {
+		t.Fatal("Struct assertion had failed")
+	}
+	t.Logf("Luxury car has %d doors.\n", luxuryCar.NumDoors())
+}
